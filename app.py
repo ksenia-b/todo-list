5b1fcd724b0aba8606ee9e06cfed96022ab1da8c
Oksana <ksenia.barshchyk@yandex.ru>
2020-03-03 22:25:21 +0200

    added token required decorator

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -33,8 +36,31 @@ class Todo(db.Model):
     user_id = db.Column(db.Integer)
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.header:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
 @app.route('/user', methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
     users = User.query.all()
 
     output = []
@@ -52,7 +78,8 @@ def get_all_users():
 
 
 @app.route('/user/<public_id>', methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
     user = User.query.filter_by(public_id = public_id).first()
 
     if not user:
@@ -68,7 +95,8 @@ def get_one_user(public_id):
 
 
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
     data = request.get_json()
     print("data = ", data)
     hashed_password = generate_password_hash(data['password'], method='sha256')
@@ -82,7 +110,8 @@ def create_user():
 
 
 @app.route('/user/<public_id>', methods=['PUT'])
-def promote_user(public_id):
+@token_required
+def promote_user(current_usesr, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -96,7 +125,8 @@ def promote_user(public_id):
 
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -107,10 +137,26 @@ def delete_user(public_id):
 
     return jsonify({'message': 'The user has been deleted!'})
 
-@app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user():
-    return ''
 
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                           app.config['SECRET_KEY'])
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!'})
 
 if __name__ == '__main__':
     # manager.run()
