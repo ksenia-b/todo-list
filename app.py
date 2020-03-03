71b9cca6385f63eca692f1fdabb1f0d0f10f4bb6
Oksana <ksenia.barshchyk@yandex.ru>
2020-03-03 21:48:10 +0200

    added 'get one user'

--- app.py
+++ app.py
@@ -35,12 +35,36 @@ class Todo(db.Model):
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return ''
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+
+        output.append(user_data)
+
+    return jsonify({'users': output})
 
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    user = User.query.filter_by(public_id = public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user': user_data})
 
 
 @app.route('/user', methods=['POST'])
@@ -57,17 +81,17 @@ def create_user():
     return jsonify({"message": "new user created"})
 
 
-@app.route('/user/<user_id>', methods=['PUT'])
+@app.route('/user/<public_id>', methods=['PUT'])
 def promote_user():
     return ''
 
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
     return ''
 
 
 if __name__ == '__main__':
-    manager.run()
-    # app.run(debug=True)
+    # manager.run()
+    app.run(debug=True)
 
